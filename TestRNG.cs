cfb0d7ee0395fc37cd9a0200bac91a2cfbe85c68
tomast1337 <vycasnicolas@gmail.com>
2026-02-19 11:44:48 -0300

    updated safety

--- TestRNG.cs
+++ TestRNG.cs
@@ -23,6 +23,7 @@ public class JavaRandom
 {
     private static long _seedUniquifier = 8682522807148012L;
     private readonly object _lock = new object();
+    private readonly object _seedLock = new object();
     private bool _haveNextNextGaussian = false;
     private double _nextNextGaussian;
     private const float FloatUnit = 1.0f / (1 << 24);
@@ -56,15 +57,16 @@ public class JavaRandom
         return (seed ^ Multiplier) & Mask;
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     private int Next(int bits)
     {
-        long oldSeed, nextSeed;
-        do
+        long nextSeed;
+
+        lock (_seedLock)
         {
-            oldSeed = Interlocked.Read(ref _seed);
-            nextSeed = (oldSeed * Multiplier + Addend) & Mask;
-        } 
-        while (Interlocked.CompareExchange(ref _seed, nextSeed, oldSeed) != oldSeed);
+            _seed = (_seed * Multiplier + Addend) & Mask;
+            nextSeed = _seed;
+        }
 
         return (int)((ulong)nextSeed >> (48 - bits));
     }
